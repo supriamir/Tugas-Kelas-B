fde378c4d8a5566aa5f1cd99595b6584a68ad879
alba054 <54732727+alba054@users.noreply.github.com>
2019-09-16 08:10:35 +0800

    Update BilanganRealLooping.java
    nambah comment

--- H071191040/BilanganRealLooping.java
+++ H071191040/BilanganRealLooping.java
@@ -3,32 +3,35 @@ class BilanganRealLooping
 {
   public static void main(String[] args)
   {
-    int odd=0,even=0,pos=0,neg=0;
+    int odd=0;
+    int even=0;
+    int pos=0;
+    int neg=0;
     Scanner key = new Scanner(System.in);
     for(int i = 0 ; i < 5 ; i++)
     {
       int n = key.nextInt();
-      if(n % 2 == 0 && n > 0)
+      if(n % 2 == 0 && n > 0) /* mengecek apakah bilangan genap dan positif */
       {
         even++;
         pos++;
       }
-      else if(n % 2 == 0 && n < 0)
+      else if(n % 2 == 0 && n < 0) /* mengecek apakah bilangan genap dan negatif */
       {
         neg++;
         even++;
       }
-      else if(n % 2 != 0 && n > 0)
+      else if(n % 2 != 0 && n > 0) /* mengecek apakah bilangan ganjil dan positif */
       {
         odd++;
         pos++;
       }
-      else if(n % 2 != 0 && n < 0)
+      else if(n % 2 != 0 && n < 0) /* mengecek apakah bilangan ganjil dan negatif */
       {
         odd++;
         neg++;
       }
-      else
+      else /* mengecek apakah nol */
       {
         even++;
       }

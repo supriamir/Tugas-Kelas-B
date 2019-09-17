e245412dafc2b02f0cbf91d23fe39d71c0a88833
SakinahYunus <55241291+SakinahYunus@users.noreply.github.com>
2019-09-16 23:23:03 -0700

    Update Nomor1.java

--- H071191046/Nomor1.java
+++ H071191046/Nomor1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Nomor1{
     public static void main(String[] args) {
     Scanner nmr = new Scanner(System.in);
-    System.out.println("N = ");
+    System.out.print("N = ");
     int N = nmr.nextInt();
     System.out.print("Faktor dari " + N + " adalah " + " "); 
     for (int i = 1; i <= N; i++){

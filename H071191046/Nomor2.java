d7df93d8e6f7c304982f94d007a7b82e4f87652c
SakinahYunus <55241291+SakinahYunus@users.noreply.github.com>
2019-09-16 23:24:32 -0700

    Update Nomor2.java

--- H071191046/Nomor2.java
+++ H071191046/Nomor2.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Nomor2 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    System.out.print("N = ");    
     int N = sc.nextInt();
     int sum = 0;
     

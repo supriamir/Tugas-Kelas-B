2d8acac03cf169d5e8d4f32e6d4b8da99d1d4157
SakinahYunus <55241291+SakinahYunus@users.noreply.github.com>
2019-09-16 23:26:22 -0700

    Update Nomor4.java

--- H071191046/Nomor4.java
+++ H071191046/Nomor4.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Nomorr4 {
+public class Nomor4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int genap = 0;

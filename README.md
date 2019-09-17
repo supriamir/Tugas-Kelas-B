4c3c6a3eae2dec24e088f33114be2625aceb3ac5
lopintasarungallo <lopinsarungallo@gmail.com>
2019-09-17 20:50:50 +0800

    H071191024

--- README.md
+++
@@ -1,1 +0,0 @@
-# Tugas-Kelas-B
---
+++ tugaslooping3.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class tugaslooping3
+{
+  public static void main(String[] args)
+  {
+      //bilangan ganjil
+    Scanner key = new Scanner(System.in);
+    int n = key.nextInt();
+    int odd = 0;
+    System.out.printf("bilangan ganjil = ");
+    if(n > 0)
+    {
+      for(int i = 1; i <= n; i++)
+      {
+        if(i % 2 != 0)
+        {
+          odd++;
+          System.out.printf("%d ",i);
+        }
+
+      }
+    }
+    else if(n < 0)
+    {
+      for(int i = 0; i >= n ; i--)
+      if(i % 2 != 0)
+      {
+        odd++;
+        System.out.printf("%d ",i);
+      }
+    }
+    else
+    {
+      System.out.printf("gak ada");
+    }
+
+    System.out.println("");
+    System.out.printf("jumlah bilangan ganjil = %d",odd);
+  }
+}

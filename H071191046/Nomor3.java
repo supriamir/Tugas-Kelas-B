8f5cbf9dc9b485b5d14db37bd7327cd3723cc09b
SakinahYunus <55241291+SakinahYunus@users.noreply.github.com>
2019-09-16 23:26:01 -0700

    Update Nomor3.java

--- H071191046/Nomor3.java
+++ H071191046/Nomor3.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class Nomor3{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("N = ");
         int N = sc.nextInt();
+        System.out.print("Bilangan Ganjil = ");
         int sum = 0;
         for (int i = 1; i <= N; i+=2) {
             System.out.print(i + " , ");

81daca8c326ee32a56eb181127e877053805766e
taufiqalifff <taufiqalif415@gmail.com>
2019-09-18 22:21:38 +0800

    Tugas

---
+++ H071191010/Tugas 1/Tugas2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Tugas2{
+  public static void main(String[] args)
+  {
+    Scanner key = new Scanner(System.in);
+    int a = key.nextInt();
+    System.out.printf("%d merupakan kelipatan dari ",a);
+    if(a > 0)
+    {
+      for(int i = 1; i <= a; i++)
+      {
+        if(a % i == 0)
+        {
+          System.out.printf("%d ",i);
+        }
+      }
+    }
+    else if(a < 0)
+    {
+      for(int i = 1; i <= (-1*a) ; i++)
+      {
+        if(a % i == 0)
+        {
+          System.out.printf("%d ",i);
+        }
+      }
+    }
+    else
+    {
+      System.out.printf("null");
+    }
+
+
+  }
+}

226f9a7b06de856cd8499e53f31ded3cd5015edb
irenemaritsa <irenemaritsa19@gmail.com>
2019-09-23 13:53:12 +0800

    Tugas perulangan

---
+++ Tugas 1/Tpp2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Tpp2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int password = 2002;
+        boolean pass = false;
+        while (pass == false){
+            int inputPassword = sc.nextInt();
+            if (inputPassword == password){
+                System.out.println("password benar");
+                pass = true;
+
+            } else {
+                System.out.println("password salah");
+            }
+            
+        }
+        
+    }
+}

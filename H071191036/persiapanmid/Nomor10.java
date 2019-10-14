b60d6fd163a884c310d06b8b30bea0577c9ace26
bayu-17ajid <bayuajid12@gmail.com>
2019-10-14 22:28:46 +0800

    persiapan mid

---
+++ H071191036/persiapanmid/Nomor10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Nomor10
+{
+  public static void main(String[] args)
+  {
+    Scanner read = new Scanner(System.in);
+    int m = read.nextInt();
+    int n = read.nextInt();
+    int matrice[][] = new int[m][n];
+    for(int i = 0; i < m; i++)
+    {
+      for(int j = 0; j < n; j++)
+      {
+        matrice[i][j] = read.nextInt();
+      }
+    }
+    for(int i = 0; i < n; i++)
+    {
+      for(int j = m-1; j >= 0; j--)
+      {
+        System.out.print(matrice[j][i] + " ");
+      }
+      System.out.println();
+    }
+  }
+}
---
+++ H071191036/persiapanmid/Nomor13.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class Nomor13
+{
+  public static void main(String[] args)
+  {
+    Scanner read = new Scanner(System.in);
+    int t = read.nextInt(); // jumlah kasus
+    int sum = 0;
+    int avg[] = new int[t]; // rata-rata dalam setiap kasus
+    //memasukkan nilai nilai mahasiswa sebanyak kasus
+    int count[] = new int[t]; //banyak mahasiswa yang bermasalah tiap kasus
+    for(int i = 0; i < t; i++)
+    {
+      int n = read.nextInt();
+      int nilai[] = new int[n]; //nilai mahasiswa sebanyak n mahasiswa
+
+      for(int j = 0; j < n; j++)
+      {
+        nilai[j] = read.nextInt(); //input nilai mahasiswa
+        sum += nilai[j];
+      }
+      sum /= n;
+      avg[i] = sum; //rata2 nilai mahasiswa dalam setiap kasus
+      sum = 0;
+      count[i] = 0;
+      for(int l = 0; l < n; l++)
+      {
+        count[i] += nilai[l] < avg[i] ? 1:0; //menghitung jumlah mahasiswa yang bermasalah
+      }
+    }
+    for(int i = 0; i < t; i++)
+    {
+      System.out.println(count[i]);
+    }
+  }
+}
